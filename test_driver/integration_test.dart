edffee6ecf64322b3a47873eda812422f384ef0b
Lenovo User <lenovo@example.com>
2026-09-19 03:17:00 +0530

    Configure integration_test driver for automated screenshot capture

---
+++ test_driver/integration_test.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:integration_test/integration_test_driver_extended.dart';
+
+Future<void> main() async {
+  await integrationDriver(
+    onScreenshot: (String screenshotName, List<int> screenshotBytes, [Map<String, dynamic>? args]) async {
+      final File image = File('screenshots/$screenshotName.png');
+      image.parent.createSync(recursive: true);
+      image.writeAsBytesSync(screenshotBytes);
+      print('Saved screenshot: ${image.path}');
+      return true;
+    },
+  );
+}
